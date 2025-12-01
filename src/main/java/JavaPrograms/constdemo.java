4f2acc8e62f5c68ffacd15826792b10f079f7524
unknown <tajinder.singh28@gmail.com>
2025-12-01 22:01:54 +0530

    add constructor class

---
+++ src/main/java/JavaPrograms/constdemo.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+
+public class constdemo {
+	 int value;
+     String name;
+     public constdemo(int initialValue, String initialName)
+     {
+         System.out.println("this is my first construtor");    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		constdemo cd=new constdemo(12,"Rohan");
+		
+	}
+
+}
--- src/main/java/JavaPrograms/loopdemo.java
+++ src/main/java/JavaPrograms/loopdemo.java
@@ -4,6 +4,7 @@ public class loopdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		int k=1;
 		for(int i=0;i<=4;i++)
 		{
